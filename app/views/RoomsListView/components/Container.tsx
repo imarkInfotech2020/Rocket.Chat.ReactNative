imarkInfotech2020/Rocket.Chat.ReactNative

commit 32475308758e60e5a6ef1e088b3aa55c383f9a8f

chore: Use StatusBar only on root (#6526)

app/views/RoomsListView/components/Container.tsx
@@ -9,7 +9,6 @@ const Container = ({ children }: { children: ReactElement }) => {
 	const { colors } = useTheme();
 	return (
 		<SafeAreaView testID='rooms-list-view' style={{ backgroundColor: colors.surfaceRoom }}>
-			<StatusBar />
 			<TabletHeader />
 			{children}
 		</SafeAreaView>

app/containers/FormContainer.tsx
@@ -5,7 +5,6 @@ import sharedStyles from '../views/Styles';
 import scrollPersistTaps from '../lib/methods/helpers/scrollPersistTaps';
 import KeyboardView from './KeyboardView';
 import { useTheme } from '../theme';
-import StatusBar from './StatusBar';
 import AppVersion from './AppVersion';
 import { isTablet } from '../lib/methods/helpers';
 import SafeAreaView from './SafeAreaView';
@@ -39,7 +38,6 @@ const FormContainer = ({ children, testID, showAppVersion = true, ...props }: IF
 
 	return (
 		<KeyboardView>
-			<StatusBar />
 			<ScrollView
 				style={sharedStyles.container}
 				contentContainerStyle={[sharedStyles.containerScrollView, styles.scrollView]}

app/ee/omnichannel/views/QueueListView.tsx
@@ -9,7 +9,6 @@ import RoomItem from '../../../containers/RoomItem';
 import { getUserSelector } from '../../../selectors/login';
 import { useTheme } from '../../../theme';
 import SafeAreaView from '../../../containers/SafeAreaView';
-import StatusBar from '../../../containers/StatusBar';
 import { goRoom } from '../../../lib/methods/helpers/goRoom';
 import * as HeaderButton from '../../../containers/Header/components/HeaderButton';
 import { events, logEvent } from '../../../lib/methods/helpers/log';
@@ -110,7 +109,6 @@ const QueueListView = React.memo(() => {
 
 	return (
 		<SafeAreaView testID='queue-list-view' style={{ backgroundColor: colors.surfaceRoom }}>
-			<StatusBar />
 			<FlatList
 				ref={getScrollRef}
 				data={queued}

app/index.tsx
@@ -37,6 +37,7 @@ import { initStore } from './lib/store/auxStore';
 import { TSupportedThemes, ThemeContext } from './theme';
 import ChangePasscodeView from './views/ChangePasscodeView';
 import ScreenLockedView from './views/ScreenLockedView';
+import StatusBar from './containers/StatusBar';
 
 enableScreens();
 initStore(store);
@@ -227,6 +228,7 @@ export default class Root extends React.Component<{}, IState> {
 								<GestureHandlerRootView>
 									<KeyboardProvider>
 										<ActionSheetProvider>
+											<StatusBar />
 											<AppContainer />
 											<TwoFactor />
 											<ScreenLockedView />

app/lib/methods/helpers/theme.ts
@@ -57,7 +57,7 @@ export const setNativeTheme = (themePreferences: IThemePreference) => {
 			// Do nothing
 		}
 	}
-	SystemUI.setBackgroundColorAsync(themes[theme].surfaceLight);
+	SystemUI.setBackgroundColorAsync(themes[theme].surfaceNeutral);
 };
 
 export const unsubscribeTheme = () => {

app/views/AccessibilityAndAppearanceView/index.tsx
@@ -6,7 +6,6 @@ import Switch from '../../containers/Switch';
 import * as HeaderButton from '../../containers/Header/components/HeaderButton';
 import * as List from '../../containers/List';
 import SafeAreaView from '../../containers/SafeAreaView';
-import StatusBar from '../../containers/StatusBar';
 import I18n from '../../i18n';
 import { AccessibilityStackParamList } from '../../stacks/types';
 import { useAppSelector } from '../../lib/hooks';
@@ -49,7 +48,6 @@ const AccessibilityAndAppearanceView = () => {
 	}, []);
 	return (
 		<SafeAreaView>
-			<StatusBar />
 			<List.Container testID='accessibility-view-list'>
 				<List.Section>
 					<List.Separator />

app/views/AddChannelTeamView.tsx
@@ -5,7 +5,6 @@ import { useSelector } from 'react-redux';
 import { CompositeNavigationProp } from '@react-navigation/core';
 
 import * as List from '../containers/List';
-import StatusBar from '../containers/StatusBar';
 import SafeAreaView from '../containers/SafeAreaView';
 import I18n from '../i18n';
 import { ChatsStackParamList, DrawerParamList, NewMessageStackParamList } from '../stacks/types';
@@ -61,7 +60,6 @@ const AddChannelTeamView = () => {
 
 	return (
 		<SafeAreaView testID='add-channel-team-view'>
-			<StatusBar />
 			<List.Container>
 				<List.Separator />
 				{canCreateNew ? (

app/views/AddExistingChannelView/index.tsx
@@ -11,7 +11,6 @@ import I18n from '../../i18n';
 import log, { events, logEvent } from '../../lib/methods/helpers/log';
 import SearchBox from '../../containers/SearchBox';
 import * as HeaderButton from '../../containers/Header/components/HeaderButton';
-import StatusBar from '../../containers/StatusBar';
 import { useTheme } from '../../theme';
 import SafeAreaView from '../../containers/SafeAreaView';
 import { sendLoadingEvent } from '../../containers/Loading';
@@ -153,7 +152,6 @@ const AddExistingChannelView = () => {
 
 	return (
 		<SafeAreaView testID='add-existing-channel-view'>
-			<StatusBar />
 			<FlatList
 				data={channels}
 				extraData={channels}

commit 16698e96f14a428a5093b3fc6ba768a5e036a4c4

chore: Lint failing after #6526 (#6534)

app/views/RoomsListView/components/Container.tsx
@@ -1,7 +1,6 @@
 import { memo, ReactElement } from 'react';
 
 import SafeAreaView from '../../../containers/SafeAreaView';
-import StatusBar from '../../../containers/StatusBar';
 import { useTheme } from '../../../theme';
 import TabletHeader from './TabletHeader';
 